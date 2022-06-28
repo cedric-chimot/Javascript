a4e23b1d374f05cfb7c5078c6f953fdf87d426d8
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-06-28 16:30:57 +0200

    boucles

--- boucles/boucles.js
+++ boucles/boucles.js
@@ -1,7 +1,10 @@
 let i = 0;
 
-while(i < 6)
+while(i < 4)
 {
-    document.write("Je m'appelle : ");
+    document.write("Je m'appelle Cédric. ");
     i++;
 }
+
+/*break > arrêter une boucle*/
+/*continue > relancer une boucle*/
